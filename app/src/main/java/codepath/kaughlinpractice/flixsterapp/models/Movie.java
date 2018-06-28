503a5ceadb49c25464bf6d7ebd506f66cd3998b8
Kaughlin Caver <kaughlin@fb.com>
2018-06-28 15:32:10 -0700

    added ratings

--- app/src/main/java/codepath/kaughlinpractice/flixsterapp/models/Movie.java
+++ app/src/main/java/codepath/kaughlinpractice/flixsterapp/models/Movie.java
@@ -2,13 +2,20 @@ package codepath.kaughlinpractice.flixsterapp.models;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
+@Parcel // annotation indicates class is Parcelable
 public class Movie {
     // values from API
-    private String title;
-    private String overview;
-    private String posterPath; // only the path
-    private String backdropPath;
+    // fields must be public for parceler
+    String title;
+    String overview;
+    String posterPath; // only the path
+    String backdropPath;
+    Double voteAverage;
+
+    // no-arg, empty constructor required for Parceler
+    public Movie() {}
 
     //intialize from JSON data
     public Movie(JSONObject object) throws JSONException {
@@ -16,6 +23,8 @@ public class Movie {
         overview = object.getString("overview");
         posterPath = object.getString("poster_path");
         backdropPath = object.getString("backdrop_path");
+
+        voteAverage = object.getDouble("vote_average");
 
     }
 
@@ -33,5 +42,9 @@ public class Movie {
 
     public String getBackdropPath() {
         return backdropPath;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
     }
 }
